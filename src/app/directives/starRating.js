752b2ff35a8cf190ea0932b4346d9b9165779cd0
Ahmed Kamel Taha <ahmedengu@gmail.com>
2017-03-25 21:12:51 +0200

    rating

--- src/app/directives/starRating.js
+++ src/app/directives/starRating.js
@@ -23,7 +23,7 @@
                     if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
                         attrs.maxRating = '5';
                     }
-                    ;
+
                 },
                 controller: function ($scope, $element, $attrs) {
                     $scope.maxRatings = [];
@@ -31,7 +31,7 @@
                     for (var i = 1; i <= $scope.maxRating; i++) {
                         $scope.maxRatings.push({});
                     }
-                    ;
+
 
                     $scope._rating = $scope.rating;
 
